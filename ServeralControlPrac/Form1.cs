3ae838182bbc2431a792b0cb2d9fa2a74e440c56
dusdn02 <lyw9114@naver.com>
2020-07-21 09:43:36 +0900

    linklabel 주소 설정

--- ServeralControlPrac/Form1.cs
+++ ServeralControlPrac/Form1.cs
@@ -16,5 +16,10 @@ namespace ServeralControlPrac
         {
             InitializeComponent();
         }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://google.co.kr");
+        }
     }
 }
